1daabbaf50187dff5db717fe880c6da7401f62eb
ganzjb11 <verenjunise@gmail.com>
2025-09-16 07:59:48 +0700

    Update api/create-panel.js

--- api/create-panel.js
+++ api/create-panel.js
@@ -94,12 +94,8 @@ module.exports = async (req, res) => {
                 nest: parseInt(nestId),
                 egg: parseInt(eggId),
                 docker_image: "ghcr.io/parkervcp/yolks:nodejs_18",
-                // --- DIKEMBALIKAN KE VERSI ASLI KAMU ---
                 startup: "if [[ -d .git ]] && [[ {{AUTO_UPDATE}} == \"1\" ]]; then git pull; fi; if [[ ! -z ${NODE_PACKAGES} ]]; then /usr/local/bin/npm install ${NODE_PACKAGES}; fi; if [[ ! -z ${UNNODE_PACKAGES} ]]; then /usr/local/bin/npm uninstall ${UNNODE_PACKAGES}; fi; if [ -f /home/container/package.json ]; then /usr/local/bin/npm install; fi; if [[ ! -z ${CUSTOM_ENVIRONMENT_VARIABLES} ]]; then vars=$(echo ${CUSTOM_ENVIRONMENT_VARIABLES} | tr \";\" \"\\n\"); for line in $vars; do export $line; done fi; /usr/local/bin/${CMD_RUN};",
-                environment: {
-                    "CMD_RUN": "npm start"
-                },
-                // -----------------------------------------
+                environment: { "CMD_RUN": "npm start" },
                 limits: { memory, swap: 0, disk, io: 500, cpu },
                 feature_limits: { databases: 1, allocations: 1, backups: 1 },
                 deploy: { locations: [parseInt(locationId)], dedicated_ip: false, port_range: [] },
