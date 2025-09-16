90d7dcee34a1acac22f371d7b48fcd94e5e21f7d
ganzjb11 <verenjunise@gmail.com>
2025-09-16 08:00:13 +0700

    Update api/getUserRole.js

--- api/getUserRole.js
+++ api/getUserRole.js
@@ -3,9 +3,7 @@ const { verifyUser } = require('./_firebase-admin.js');
 
 module.exports = async (req, res) => {
     try {
-        // Cukup verifikasi kalau dia user yang valid, tidak perlu role khusus
         const { userData } = await verifyUser(req); 
-        // Kirim balik hanya data role-nya
         res.status(200).json({ role: userData.role });
     } catch (error) {
         res.status(401).json({ message: error.message });
